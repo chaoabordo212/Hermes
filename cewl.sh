0324dd36aaf0b4dc0e069e48221c144426b97edd
united <chaoabordo@gmail.com>
2021-01-23 09:15:36 +0100

    Overwritten old cewl.sh

--- cewl.sh
+++ cewl.sh
@@ -2,9 +2,25 @@
 
 # - Bash script for automating web page word collection and making a wordlist
 
+
+verbose='false'
+minwordlength='5'
+depth=3
+offsite='false'
+includemail='false'
+excludelist='false'
+includelist='false'
+lowercase='false'
+convumlaut='false'
+
 targeturl=''
 urllist=''
-verbose='false'
+offsite_flag=''
+exclude_flag=''
+include_flag=''
+lowercase_flag=''
+convumlaut_flag=''
+mail_flag=''
 
 DateAndTime() { DateAndTime=$( date '+%d-%m-%Y | %H:%M:%S' ); echo -e "$DateAndTime"; }
 
@@ -18,7 +34,7 @@ function CewlCrawl {
 	dirname='RESULTS/'$( echo $targeturl | cut -d "/" -f3 )
 	mkdir -p $dirname
 	resultfile=$dirname'/'$( echo $targeturl | cut -d "/" -f3 )".txt"
-	sudo cewl -w $resultfile -d 3 -o -m 8 --lowercase --convert-umlauts $ver_flag $targeturl;
+	sudo cewl -d $depth -m $minwordlength $offsite_flag $exclude_flag $include_flag -w $resultfile $lowercase_flag $convumlaut_flag $mail_flag $verbose_flag $targeturl;
 	
 	wordcount=$(< $resultfile wc -l)
 	echo "Finished crawling: "$line
@@ -27,12 +43,53 @@ function CewlCrawl {
 	echo ""
 }
 
-
-while getopts 't:f:v' flag; do
+count=0
+while getopts 'm:d:o:E:I:l:c:e:t:f:v' flag; do
 	case "${flag}" in
+
+    # Min word length
+    m) minwordlength="${OPTARG}"
+	echo "minwordlength_debug: " $minwordlength ###DEBUG
+	;;
+
+	# Depth of search
+	d) depth="${OPTARG}"
+	echo "depth_debug: " $depth ###DEBUG
+	;;
+
+	# Search offsite
+	o) offsite="true"
+	offsite_flag="-o"
+	;;
+
+	# Exclude list location
+	E) excludelist="${OPTARG}"
+	exclude_flag="--exclude $excludelist"
+	;;
+
+	# Include list location
+	I) includelist="${OPTARG}"
+	include_flag="--include $includelist"
+	;;
+
+	# Convert all to lowercase
+	l) lowercase="true"
+	lowercase_flag="--lowercase"
+	;;
+
+	# Sanitize umlaut
+	c) convumlaut="true"
+	convumlaut_flag="--convert-umlauts"
+	;;
+
+	# Include e-mail addresses
+	e) includemail="true"
+	mail_flag="-e"
+	;;	
 
 	# Single URL
 	t) targeturl="${OPTARG}"
+	echo "Crawling URL: " $targeturl
 	CewlCrawl
 	;;
 
@@ -43,23 +100,26 @@ while getopts 't:f:v' flag; do
 	input=$urllist
 	while IFS= read -r line
 	do
+		((count=count+1))
 		targeturl=$line
 		CewlCrawl
-
+		echo "Finished URL:"
+		echo "$Line"
+		if [ "$count" == "$pageno" ]; then
+			echo "All URL on the list finished"
+			exit 1
+		fi
+		echo "Continuing to the next line"
 	done < "$input"
-	echo "All URL on the list finished"
 	;;
-
+	
 	# Verbose output
     v) verbose="true"
-	ver_flag="-v"
+	verbose_flag="-v"
     ;;
-    *) print_usage
-       exit 1 ;;
 
+	*) print_usage
+       exit 1 ;;
   	esac
 done
-
-
-
 
